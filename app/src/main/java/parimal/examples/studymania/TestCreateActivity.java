d58214883f519fc6b04c1c249abbffc5450251be
ParimalKesan <parimalkesan3@gmail.com>
2020-05-10 22:06:29 +0530

    The mainactivity class is used to set button for displaying popup.The SubjectSelectionDialogFragment is used to build a dialog with radiobuttons for different subjects and set action for dialog buttons. The TestcreateActivity is used to select number of qns and total marks which is afterwrds redirected to WebActivity which displays a webview with url of google.com

--- app/src/main/java/parimal/examples/studymania/TestCreateActivity.java
+++ app/src/main/java/parimal/examples/studymania/TestCreateActivity.java
@@ -1,6 +1,7 @@
 package parimal.examples.studymania;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +11,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class TestCreateActivity extends AppCompatActivity {
+public class TestCreateActivity extends AppCompatActivity implements LevelSelectDialogFragment.LevelSelectListener {
 
     private EditText qn_number_edittext;
     private Button qn_plus_button;
     private Button qn_minus_button;
     private EditText total_marks_edittext;
     private Button submit_button;
+    String levelselected="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +52,7 @@ public class TestCreateActivity extends AppCompatActivity {
             }
         });
 
-        //submit button to submit the no of questions and total marks and redirect to a webview
+        //submit button to submit the no of questions and total marks and open a dialog to set difficulty level
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,7 +60,10 @@ public class TestCreateActivity extends AppCompatActivity {
                 int total_marks=Integer.valueOf(total_marks_edittext.getText().toString());
                 //method to evaluate marks per question
                 findMarksPerQuestion(total_no_of_qns,total_marks);
-                startActivity(new Intent(TestCreateActivity.this,WebActivity.class));
+                //create a new object of SubjectSelectDialogFragment and show the created dialog
+                DialogFragment levelSelectDialog=new LevelSelectDialogFragment();
+                levelSelectDialog.setCancelable(false);
+                levelSelectDialog.show(getSupportFragmentManager(),"LevelSelectDialog");
             }
         });
     }
@@ -68,5 +73,23 @@ public class TestCreateActivity extends AppCompatActivity {
     {
         double marksperquestion=(double)totalmarks/(double)totalqns;
         Log.d("test", "findMarksPerQuestion: "+marksperquestion);
+    }
+
+    //implement methods from LevelSelectListener interface positive button clicked
+    @Override
+    public void onPositiveButtonClicked(String[] level_list, int Position) {
+        //find the level selected from dialog box and pass it to an intent
+        levelselected=level_list[Position];
+        Intent intent1=new Intent(TestCreateActivity.this,WebActivity.class);
+        intent1.putExtra("level",levelselected);
+        //start the WebActivity class to display google.com
+        startActivity(intent1);
+
+    }
+
+    //implement methods from LevelSelectListener interface negative button clicked
+    @Override
+    public void onNegativeButtonClicked() {
+
     }
 }
