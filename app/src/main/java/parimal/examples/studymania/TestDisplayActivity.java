304644e99cc87bcd80a2a71b35b758252918266e
ParimalKesan <parimalkesan3@gmail.com>
2020-05-18 23:32:22 +0530

    The mainactivity class has a create test button which oncliked opens a dialog fragemnt to take the subject name.this is maanged by subjectselectdialogfragment. On positivebuttonclicked opens testcreateactivity to take totalnoofqns and totalmarks.after clicking on submit button, the dialogfragment opens to take difficulty level which is managed by levelselectdialogfragment. On positivebuttonclicked opens testdisplayactivity.

---
+++ app/src/main/java/parimal/examples/studymania/TestDisplayActivity.java
@@ -0,0 +1,17 @@
+package parimal.examples.studymania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class TestDisplayActivity extends AppCompatActivity {
+    private WebView set_qns_webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_display);
+
+    }
+}
